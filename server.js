beaa7af276c604f8a9cd337649ce9d4cde82b76f
Robert Schwartz <bschwartz23@gmail.com>
2021-06-17 19:27:54 -0700

    install vital npm packages, set up server.js file

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
+// Required Packages
+// =============================================
+const path = require('path');
 const express = require('express');
-const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
-
-
-
-
-
-
-
-
+// Set up Express
 const app = express();
 const PORT = process.env.PORT || 3001;
+// TODO:  we need to give this a new PORT from HEROKU
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Session
+// ==============================================
+const sess = {
+    // secret: 'Super secret secret',
+    // cookie: {},
+    // resave: false,
+    // saveUninitialized: true,
+    // store: new SequelizeStore({
+    //     db: sequelize
+    // })
+};
+
+// Use Session
+app.use(session(sess));
+
+// Handlebars
+// ================================================
+// Helpers
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+// Handlebars Engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use Controllers
+app.use(require('./controllers/'));
+
+// Listen for Port
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
