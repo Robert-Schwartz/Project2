69fcdf8a163727a23a273f932a4ba008bd0d9bab
Eliadad <eliasashtonsaunders@gmail.com>
2021-06-20 14:01:09 -0700

    started edits

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -5,6 +5,7 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const profPic = document.querySelector('')
 
     // POST the username, email, and password from the form to our server,
     if (username && email && password) {
@@ -15,6 +16,7 @@ async function signupFormHandler(event) {
                 email,
                 password
             }),
+            profilePicture,
             headers: { 'Content-Type': 'application/json' }
         });
         // check the response status
