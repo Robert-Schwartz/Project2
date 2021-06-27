15e243c1d04e5554b9fe4e81bdcdd958778e0065
samuel Odubamowo <wemsamuel@gmail.com>
2021-06-27 16:12:29 -0700

    style

--- public/javascript/findMyFriend.js
+++
@@ -1,9 +0,0 @@
-async function findMyFriendFormHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('.find-my-friend').value;
-        document.location.replace(`/${username}`);
- 
-}
-
-document.querySelector('.find-friend-submit').addEventListener('click', findMyFriendFormHandler);
