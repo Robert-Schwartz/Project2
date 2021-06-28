4334d324f963e2ce20d3312f9cc1f815ca001eea
Eliadad <eliasashtonsaunders@gmail.com>
2021-06-27 20:02:16 -0700

    this operates 100% on local

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const seedGames = require('./game-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
 
     await seedGames();
 
